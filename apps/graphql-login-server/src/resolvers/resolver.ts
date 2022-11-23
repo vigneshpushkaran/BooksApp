988ec9ca92c0bfaeff873288d380d5970d0a123e
vignesh-pushkaran <vignesh.pushkaran@lloydsbankinggroup.com>
2022-11-23 15:47:21 +0000

    added router and union options

--- apps/graphql-login-server/src/resolvers/resolver.ts
+++ apps/graphql-login-server/src/resolvers/resolver.ts
@@ -12,5 +12,22 @@ export const resolvers = {
         id: faker.random.alphaNumeric(10)
       };
     },
+    signUp: (_parent, args, _ctx) => {
+      if(args.userName){
+        return {
+          __typename: "signUp",
+          userName: args.userName,
+          id: faker.random.alphaNumeric(10),
+          status: "Created",
+          message: `Welcome ${args.userName} !`
+        };
+      }
+      return{
+        __typename: "error",
+        message: "username can't be empty",
+        code: "400"
+      }
+
+    },
   }
 };
